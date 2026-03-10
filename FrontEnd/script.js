473b54614bd8151d9fdbd4cf47c6abe16fcdfd17
ismael mansouri <ismael.mansouri@gmail.com>
2026-03-10 12:34:58 +0000

    màj

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,33 +1,50 @@
+let works = [];
+let categories = [];
+const gallery = document.querySelector('.gallery');
+
+async function init() {
+  await afficherProjets();
+  await afficherfiltres();
+}
+
+function ajouterProjet(work) {
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const figcaption = document.createElement('figcaption');
+
+  img.src = work.imageUrl;
+  img.alt = work.title;
+  figcaption.textContent = work.title;
+
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  gallery.appendChild(figure);
+}
+
 async function afficherProjets() {
   const reponse = await fetch('http://localhost:5678/api/works');
-  const works = await reponse.json();
-
-  const gallery = document.querySelector('.gallery');
+  works = await reponse.json();
 
   for (let i = 0; i < works.length; i++) {
     const work = works[i];
-
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
-
-    img.src = work.imageUrl;
-    img.alt = work.title;
-    figcaption.textContent = work.title;
-
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+    ajouterProjet(work);
   }
 }
 
-afficherProjets();
-
 async function afficherfiltres() {
   const reponse = await fetch('http://localhost:5678/api/categories');
-  const categories = await reponse.json();
+  categories = await reponse.json();
 
   const categoriesContainer = document.querySelector('.filtres');
+
+  const buttonTous = document.querySelector('[data-categorie="tous"]');  
+  buttonTous.classList.add('active');
+  buttonTous.addEventListener('click', () => {
+  document.querySelectorAll('.filtres button').forEach(btn => btn.classList.remove('active'));
+  buttonTous.classList.add('active');
+  gallery.innerHTML = '';
+  afficherProjets(); 
+  });
 
   for (let i = 0; i < categories.length; i++) {
     const category = categories[i];
@@ -36,8 +53,24 @@ async function afficherfiltres() {
     button.textContent = category.name;
     button.dataset.categoryId = category.id;
 
+    button.addEventListener('click', () => {
+    document.querySelectorAll('.filtres button').forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+    filtrerProjets(category.id);
+  });
+
     categoriesContainer.appendChild(button);
   }
 }
 
-afficherfiltres();
+function filtrerProjets(categoryId) {
+  gallery.innerHTML = '';
+
+  for ( let i = 0 ; i < works.length; i++) {
+    if (works[i].categoryId === categoryId) {
+      ajouterProjet(works[i]);
+    }
+  }
+}
+
+init();
